f5f6b410454815ba30cab10f7ece5057d1dd9c0d
avnibobal4 <avnibobal4@gmail.com>
2026-06-12 14:53:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Contact Form Validation
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || message === "") {
+
+        alert("Please fill all fields.");
+
+        event.preventDefault();
+    }
+    else {
+
+        alert("Form Submitted Successfully!");
+    }
+});
